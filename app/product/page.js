5ea1535244e0140be019e4661085ff8d29e75f12
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-23 15:02:11 +0600

    link component query

--- app/product/page.js
+++ app/product/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menu from "@/components/menu";
 
-const Page = () => {
+const Page = ({searchParams}) => {
     return (
         <div>
             <Menu/>
             <h1>this is product page</h1>
+            <h2>Product Name:{searchParams.name}</h2>
+            <h2>Product Price{searchParams.price}</h2>
         </div>
     );
 };
--- components/menu.js
+++ components/menu.js
@@ -8,10 +8,12 @@ const Menu = () => {
     let pathname = usePathname();
     return (
         <div>
-            <Link className={pathname==='/'?"active-link":"pending-link"} href='/'>Home</Link><br/>
-            <Link className={pathname==='/about'?"active-link":"pending-link"} href='/about'>About</Link><br/>
-            <Link className={pathname==='/product'?"active-link":"pending-link"} href='/product'>Product</Link><br/>
-            <Link className={pathname==='/profile'?"active-link":"pending-link"} href='/profile'>Profile</Link>
+            <Link className={pathname==='/'?"active-link":"pending-link"}  href='/'>Home</Link><br/>
+            <Link className={pathname==='/about'?"active-link":"pending-link"}  href='/about'>About</Link><br/>
+            <Link className={pathname==='/product'?"active-link":"pending-link"}
+                  href={{pathname:'/product',query:{name:"apple",price:"180k"}}}
+            >Product</Link><br/>
+            <Link className={pathname==='/profile'?"active-link":"pending-link"}   href='/profile'>Profile</Link>
         </div>
     );
 };
