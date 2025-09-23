88e1f5f4dd2a9c4f74b81d7a52d4372ee392521d
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-23 15:09:08 +0600

    navigation in programatically way

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 
+'use client'
 import React from 'react';
 import Menu from "@/components/menu";
-
+import {useRouter} from "next/navigation";
 
 const Page = async () => {
+    const router = useRouter();
+    const go=()=>{
+        router.push('/product?name=apple&price=180k');
+    }
 
     return (
         <div>
             <Menu/>
            <h1>this is home page</h1>
+            <button onClick={go}>Go to product</button>
         </div>
     );
 };
