1185e2fd96ea5ac398af2c79f8e8bca04132c297
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-23 14:10:06 +0600

    navigation active-link

--- components/menu.js
+++ components/menu.js
@@ -1,13 +1,17 @@
+'use client'
 import React from 'react';
 import Link from "next/link";
+import '../app/globals.css'
+import {usePathname} from "next/navigation";
 
 const Menu = () => {
+    let pathname = usePathname();
     return (
         <div>
-            <Link href='/'>Home</Link><br/>
-            <Link href='/about'>About</Link><br/>
-            <Link href='/product'>Product</Link><br/>
-            <Link href='/profile'>Profile</Link>
+            <Link className={pathname==='/'?"active-link":"pending-link"} href='/'>Home</Link><br/>
+            <Link className={pathname==='/about'?"active-link":"pending-link"} href='/about'>About</Link><br/>
+            <Link className={pathname==='/product'?"active-link":"pending-link"} href='/product'>Product</Link><br/>
+            <Link className={pathname==='/profile'?"active-link":"pending-link"} href='/profile'>Profile</Link>
         </div>
     );
 };
